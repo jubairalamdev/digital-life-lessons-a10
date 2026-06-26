91524136112cc218082b476a5b210789b8762338
Jubair Alam <jubairalam.dev@gmail.com>
2026-06-26 13:36:24 +0600

    Home page created fully.

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -1,9 +1,18 @@
+import HeroBanner from "@/components/homepage/Banner";
+import WhyLifeLessons from "@/components/homepage/Benefits";
+import HomeContributors from "@/components/homepage/Contributors";
+import HomeFeatured from "@/components/homepage/Featured";
+import { TrendingLessonsSection } from "@/components/homepage/TrendingLessonsSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h4>This is the Homepage</h4>
+      <HeroBanner />
+      <HomeFeatured />
+      <WhyLifeLessons />
+      <HomeContributors />
+      <TrendingLessonsSection />
     </>
   );
 }
