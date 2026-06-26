ace760d7c02551d8b14de2b029b990bc9ad78894
Jubair Alam <jubairalam.dev@gmail.com>
2026-06-26 13:27:45 +0600

    Global Loader

---
+++ src/app/loading.js
@@ -0,0 +1,10 @@
+const loading = () => {
+    return (
+        <div className="h-[85vh] flex justify-center items-center">
+
+        <div className="loader"></div>
+        </div>
+    );
+};
+
+export default loading;
